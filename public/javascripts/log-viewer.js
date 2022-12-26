aef96f53cf98bebd5d26bfea1c1da8f2a10b05e2
Jeronimo luis Vera <jeronimo.l.vera@exxonmobil.com>
2022-12-26 11:51:15 -0300

    module 4

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,12 +1,13 @@
 const connection = new WebSocket('ws://localhost:3000');
 const logWindow = document.querySelector('#log-window');
-const filePath = document.getElementById('logFilePath');
+const filePath = document.getElementById('logFilePath').value;
 
 connection.onopen = () => {
     if (filePath) {
         connection.send(filePath);
     }
 };
+
 connection.onmessage = (event) => {
     const logs = event.data.split('\n').join('<hr>');
     logWindow.innerHTML = logs;
